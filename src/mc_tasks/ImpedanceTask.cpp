7e72699069eea5f697fd817adc9d0c352109adba
YunosukeA22 <aoforuniversity@gmail.com>
2025-07-30 11:00:56 +0900

    remove warning

--- src/mc_tasks/ImpedanceTask.cpp
+++ src/mc_tasks/ImpedanceTask.cpp
@@ -35,11 +35,6 @@ ImpedanceTask::ImpedanceTask(const mc_rbdyn::RobotFrame & frame, double stiffnes
   const auto & robot = frame.robot();
   type_ = "impedance";
   name_ = "impedance_" + robot.name() + "_" + frame.name();
-
-  // if(!frame.hasForceSensor())
-  // {
-  //   mc_rtc::log::error_and_throw("[{}] Frame {} does not have a force sensor attached", name_, frame.name());
-  // }
 }
 
 void ImpedanceTask::update(mc_solver::QPSolver & solver)
